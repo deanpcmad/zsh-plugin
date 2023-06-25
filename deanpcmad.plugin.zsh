e23404cfca6dd301fb6ca727f454112bd3e458ae
Dean Perry <deanperry@fastmail.com>
2023-06-25 16:15:01 +0100

    gr = git remote -v

--- deanpcmad.plugin.zsh
+++ deanpcmad.plugin.zsh
@@ -62,6 +62,7 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gsh="git rev-parse --short HEAD"
+alias gr="git remote -v"
 
 # Curl
 alias curlxml="curl -H 'Accept: application/xml' -H 'Content-type: application/xml' "
